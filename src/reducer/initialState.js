9e00387111a57172c2b397960b7d01957eaabc5d
chauhan-rahul <chauhan_rahul@live.com>
2018-03-19 10:30:15 +0530

    Update initialState.js

--- src/reducer/initialState.js
+++ src/reducer/initialState.js
@@ -1,17 +1,6 @@
 //This is to ensure that we can see the entirety of the store
 
 export default {
-    authorReducer: {
-        authors: []
-    },
-
-    coursesReducer: {
-        courses: []
-    },
-
-    selectedCourseReducer: {
-        course: undefined
-    },
 
     apiReducer: {
         apiCallsInProgress: 0
